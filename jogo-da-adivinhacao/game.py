41c7041d5c4e7b29c5beb4d5171ca6781cce2148
gustavo-mendel <gustavo.mendel.br@gmail.com>
2020-05-21 21:12:31 -0300

    change language for english, and create a copy with portuguese

--- jogo-da-adivinhacao/game.py
+++ jogo-da-adivinhacao/game.py
@@ -1,113 +1,120 @@
 from random import randint
 
-def apresentacao():
+def presentation():
     print("*****************************************")
-    print("*   Bem-vindo ao jogo de adivinhação!   *")
+    print("*     Welcome to the Guessing Game!     *")
     print("*****************************************")
 
-def recebeAleatorio():
+def get_random_number():
     return randint(1, 100)
 
-def recebeUsuario():
-    usuario = int(input("Pensei em um número de 1 a 100, tente advinhá-lo: "))
-    return usuario
+def get_user_number():
+    user_number = int(input("I thought of a number between 1 and 100, try to guess it: "))
+    return user_number
 
-def validaUsuario(usuario):
-    if usuario < 1 or usuario > 100:
-        print("\nVocê digitou um número INVÁLIDO!")
-        print("Tente um número entre 1 e 100.\n")
+def validate_user_number(user_number):
+    if user_number < 1 or user_number > 100:
+        print("\nYou entered an INVALID number!")
+        print("Try a number between 1 and 100\n")
         return False
     else:
         return True
 
-def recebeDificuldade():
-    print("Escolha o nível de dificuldade!")
-    print("(1) Fácil    (2) Médio    (3) Difícil\n")
+def get_difficulty():
+    print("Choose the difficulty!")
+    print("(1) Easy    (2) Medium    (3) Hard\n")
 
-    dificuldade = int(input("Qual a dificuldade? "))
+    difficulty = int(input("What is the difficulty? "))
 
-    if dificuldade not in [1, 2, 3]:
-        print("Dificuldade inválida! Tente novamente.\n")
-        dificuldade = recebeDificuldade()
+    if difficulty not in [1, 2, 3]:
+        print("Invalid difficulty! Try again.\n")
+        difficulty = get_difficulty()
     else:
-        return dificuldade
+        return difficulty
 
-def defineDificuldade(dificuldade):
-    if dificuldade == 1: return 20
-    elif dificuldade == 2: return 12
+def define_difficulty(difficulty):
+    if difficulty == 1: return 20
+    elif difficulty == 2: return 12
     else: return 6
 
-def mensagemDificuldade(dificuldade):
+def msg_difficulty(difficulty):
     print("")
-    if dificuldade == 1: 
-        print("Você selecionou a dificuldade: Fácil")
-        print("E por isso, tem 20 chances para acertar o número!")
-    elif dificuldade == 2:
-        print("Você selecionou a dificuldade: Médio")
-        print("E por isso, tem 12 chances para acertar o número!")
+    if difficulty == 1: 
+        print("You selected the difficulty: Easy")
+        print("So, you have 20 chances to get the number right!")
+    elif difficulty == 2:
+        print("You selected the difficultye: Medium")
+        print("So, you have 12 chances to get the number right!")
     else: 
-        print("Você selecionou a dificuldade: Difícil")
-        print("E por isso, tem 6 chances para acertar o número!")
+        print("You selected the difficulty: Hard")
+        print("So, you have 6 chances to get the number right!")
     print()
 
-def acertou():
-    if numeroAleatorio == numeroUsuario: 
+def win():
+    if random_number == user_number: 
         return True
     else: return False
 
-def perdeu():
-    if tentativasUsuario > tentativasMaximas: 
+def lose():
+    if user_attempts > maximum_attempts: 
         return True
     else: return False
 
-def maior():
-    if numeroAleatorio > numeroUsuario: return True
+def is_bigger():
+    if random_number > user_number: return True
     else: return False
 
-def mensagemAcertou():
-    print("Meus parabéns! Você ACERTOU!")
-    print(f'O número sorteado foi {numeroAleatorio}.\n')
-    print(f'Você acertou o número na {tentativasUsuario}ª tentativa.')
-    print(f'Obteve um total de {pontosUsuario} pontos.\n')
+def msg_win():
+    print("Congratulations! You WIN!")
+    print(f'The random number was {random_number}.\n')
+    if user_attempts == 1:
+        print(f'You got it right on {user_attempts}st attempt')
+    if user_attempts == 2:
+        print(f'You got it right on {user_attempts}nd attempt')
+    if user_attempts == 3:
+        print(f'You got it right on {user_attempts}rd attempt')
+    else:
+        print(f'You got it right on {user_attempts}th attempt')
+    print(f'Total = {user_points} points.\n')
 
-def mensagemPerdeu():
-    print("Você atingiu o número máximo de tentativas.")
-    print("E por isso, você PERDEU!! ... Tente novamente ...\n")
+def msg_lose():
+    print("You got it the maximum attempts.")
+    print("So, you LOSE!! ... Try Again ...\n")
 
-apresentacao()
+presentation()
 
-dificuldade = recebeDificuldade()
-tentativasMaximas = defineDificuldade(dificuldade)
-mensagemDificuldade(dificuldade)
+difficulty = get_difficulty()
+maximum_attempts = define_difficulty(difficulty)
+msg_difficulty(difficulty)
 
-tentativasUsuario = 1
-pontosUsuario = 1000
+user_attempts = 1
+user_points = 1000
 
-numeroAleatorio = recebeAleatorio()
+random_number = get_random_number()
 
-while tentativasUsuario <= tentativasMaximas:
-    print(f"Tentativa {tentativasUsuario}:")
-    numeroUsuario = recebeUsuario()
-    if not validaUsuario(numeroUsuario):
-        tentativasUsuario -= 1
+while user_attempts <= maximum_attempts:
+    print(f"Attempts {user_attempts}º:")
+    user_number = get_user_number()
+    if not validate_user_number(user_number):
+        user_attempts -= 1
 
-    elif not acertou():
-        print("Errou!", end=' ')
-        if maior():
-            print("Tente um número MAIOR\n")
+    elif not win():
+        print("Missed!", end=' ')
+        if is_bigger():
+            print("Try a BIGGER number.\n")
         else:
-            print("Tente um número MENOR\n")
+            print("Try a SMALLER number.\n")
 
-        pontosUsuario -= (abs(numeroUsuario - numeroAleatorio) / 2.0)
+        user_points -= (abs(user_number - random_number) / 2.0)
 
-    elif acertou():
+    elif win():
         break
     
-    tentativasUsuario += 1
+    user_attempts += 1
 
-print("\n... FIM DE JOGO! ...\n")
+print("\n... END GAME! ...\n")
 
-if acertou(): 
-    mensagemAcertou()
+if win(): 
+    msg_win()
 else: 
-    mensagemPerdeu()
+    msg_lose()
